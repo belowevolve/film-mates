f4625bd153a06c820cf3bd4d3ec66eaf845463ac
belowevolve <130287859+belowevolve@users.noreply.github.com>
2024-11-11 12:44:08 +0300

    chore: login redesign

--- pages/login.vue
+++ pages/login.vue
@@ -13,23 +13,19 @@ function signInWithOAuth(provider: 'discord' | 'google') {
 </script>
 
 <template>
-  <div class="grid h-dvh grid-rows-3 gap-4">
-    <h1 class="mt-auto text-balance text-center text-5xl font-bold">
-      Welcome to Film-Mates
-    </h1>
-    <nav class="flex flex-col items-center gap-4 ">
-      <Button @click="signInWithOAuth('discord')">
-        Sign in with Discord
-      </Button>
-      <Button @click="signInWithOAuth('google')">
-        Sign in with Google
-      </Button>
-    </nav>
-    <p class="mt-auto text-center text-sm text-gray-500">
-      Made with &lt;3 by
-      <a href="https://github.com/belowevolve" target="_blank">
-        belowevolve
-      </a>
-    </p>
-  </div>
+  <h2 class="text-balance pt-48 text-center text-5xl font-bold">
+    Welcome to Film-Mates
+  </h2>
+  <Button @click="signInWithOAuth('discord')">
+    Sign in with Discord
+  </Button>
+  <Button @click="signInWithOAuth('google')">
+    Sign in with Google
+  </Button>
+  <footer class="fixed inset-x-0 bottom-0 text-center text-sm text-gray-500">
+    Made with &lt;3 by
+    <a href="https://github.com/belowevolve" target="_blank">
+      belowevolve
+    </a>
+  </footer>
 </template>
